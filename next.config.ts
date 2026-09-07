62e9f260d8f730d123dbc27579f42c4c2f0f2fed
Stephanie Nehemiah <Stephanienehemiah06@gmail.com>
2026-09-07 13:52:18 +0100

    Configure Next.js for Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/Christiana-s-Porfolio",
+  assetPrefix: "/Christiana-s-Porfolio/",
   reactCompiler: true,
 };
 
