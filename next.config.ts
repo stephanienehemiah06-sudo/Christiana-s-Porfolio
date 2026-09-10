363557bafb22629508de9a06d321b9639bdcde26
Stephanie Nehemiah <Stephanienehemiah06@gmail.com>
2026-09-10 17:05:54 +0100

    prepare Trina's Treat for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  reactCompiler: true,
+
   images: {
     unoptimized: true,
   },
-  basePath: "/Christiana-s-Porfolio",
-  assetPrefix: "/Christiana-s-Porfolio/",
-  reactCompiler: true,
 };
 
 export default nextConfig;
